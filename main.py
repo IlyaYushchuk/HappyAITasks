bdd8426ecf375c861d0b0e689a17d4f1fb17182c
Ilya <3952081@gmail.com>
2025-03-27 23:17:43 +0300

    Added os HTTP proxies remove

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from config import settings
 from voice_handler import handle_voice_message
+import os
 
+os.environ.pop("HTTP_PROXY", None)
+os.environ.pop("HTTPS_PROXY", None)
 
+# Инициализация бота
 bot = Bot(token=settings.telegram_token)
 dp = Dispatcher(bot)
 
-
+# Регистрация обработчика голосовых сообщений
 @dp.message_handler(content_types=[types.ContentType.VOICE])
 async def voice_message_handler(message: types.Message):
     await handle_voice_message(message)
 
+# Обработчик текстовых команд
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.reply("Привет! Отправь мне голосовое сообщение, и я отвечу голосом.")
